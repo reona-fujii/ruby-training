a97445ad61e2ca56f60e74ada53fe4452c758217
EC2 Default User <ya5t4yfxwbcshb908r0s@gmail.com>
2020-12-15 09:37:27 +0000

    edit_training

--- lib/sample2.rb
+++ lib/sample2.rb
@@ -2,8 +2,7 @@ foods = ['ピーマン', 'トマト', 'セロリ']
 count = 0
 foods.each do |food|
   print "#{food}は好きですか？"
-  # answer = ['はい', 'いいえ'].sample
-  answer = 'いいえ'
+  answer = ['はい', 'いいえ'].sample
   puts answer
   count += 1
   redo if answer != 'はい' && count < 2
