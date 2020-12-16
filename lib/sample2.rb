f9cdf3ddd4012403c82b7a7059a2a8dd1516df9b
EC2 Default User <ya5t4yfxwbcshb908r0s@gmail.com>
2020-12-16 08:10:22 +0000

    edit

--- lib/sample2.rb
+++ lib/sample2.rb
@@ -1,10 +1,24 @@
-foods = ['ピーマン', 'トマト', 'セロリ']
-count = 0
-foods.each do |food|
-  print "#{food}は好きですか？"
-  answer = ['はい', 'いいえ'].sample
-  puts answer
-  count += 1
-  redo if answer != 'はい' && count < 2
-  count = 0
+# foods = ['ピーマン', 'トマト', 'セロリ']
+# count = 0
+# foods.each do |food|
+#   print "#{food}は好きですか？"
+#   answer = ['はい', 'いいえ'].sample
+#   puts answer
+#   count += 1
+#   redo if answer != 'はい' && count < 2
+#   count = 0
+# end
+
+text = <<-TEXT
+def hello(name)
+  puts "Hello, \#{name}!"
 end
+
+hello('Alice')
+
+hello('Bob')
+
+hello('Carol')
+TEXT
+puts text
+puts text.gsub(/^[ \t]+$/, '')
