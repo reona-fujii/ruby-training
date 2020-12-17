e3af0c740df1585bd31bc36d093f35e11b45696c
EC2 Default User <ya5t4yfxwbcshb908r0s@gmail.com>
2020-12-17 07:11:30 +0000

    edit

--- lib/sample2.rb
+++ lib/sample2.rb
@@ -9,16 +9,40 @@
 #   count = 0
 # end
 
-text = <<-TEXT
-def hello(name)
-  puts "Hello, \#{name}!"
+# text = <<-TEXT
+# def hello(name)
+#   puts "Hello, \#{name}!"
+# end
+
+# hello('Alice')
+
+# hello('Bob')
+
+# hello('Carol')
+# TEXT
+# puts text
+# puts text.gsub(/^[ \t]+$/, '')
+
+if '123-4567' =~ /\d{3}-\d{4}/
+    puts 'マッチしました'
+else
+    puts 'マッチしませんでした'
 end
 
-hello('Alice')
+text = '私の誕生日は1999年2月28日です。'
+if /(?<year>\d+)年(?<month>\d+)月(?<day>\d+)日/ =~ text
+    puts "#{year}/#{month}/#{day}"
+end
 
-hello('Bob')
+'1989年3月8日 2001年4月8日'.scan(/(\d+)年(\d+)月(\d+)日/)
 
-hello('Carol')
-TEXT
-puts text
-puts text.gsub(/^[ \t]+$/, '')
+text='09-1234-1234'
+
+case text
+when /^\d{3}-\d{4}$/
+    puts '郵便番号です'
+when /^\d{4}\/\d{1,2}\/\d{1,2}$/
+    puts '日付です'
+when /^\d+-\d+-\d+$/
+    puts '電話番号です'
+end
